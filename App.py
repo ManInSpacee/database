b2701e711a9b77a4b6d01dff1374434e535a84fe
paleavenger <lyrhg79@gmail.com>
2025-09-29 19:21:45 +0300

    Update

--- App.py
+++ App.py
@@ -1,30 +1,49 @@
 import sys
-from PyQt5 import QtWidgets, uic
-import psycopg2
+from PyQt5 import QtWidgets, uic, QtSql, QtCore, QtGui
 
+import psycopg2
 def get_connection():
     return psycopg2.connect(
         dbname="postgres",       # имя базы
         user="postgres",         # ваш пользователь
-        password="klim",         # ваш пароль
+        password="Tyur1234",         # ваш пароль
         host="localhost",        # или другой хост
         port="5432"              # порт PostgreSQL
     )
-
-
 def create_schema_and_tables():
     try:
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("""
+            CREATE TYPE attack_enum AS ENUM (
+            'syn_flood',
+            'udp_flood',
+            'icmp_flood',
+            'http_get_flood',
+            'http_post_flood',
+            'slowloris',
+            'dns_amplification',
+            'ntp_amplification',
+            'smurf',
+            'botnet'
+            );
+            
+            CREATE TABLE IF NOT EXISTS attack_name (
+                name attack_enum PRIMARY KEY
+            );
+            
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
-                attack_type VARCHAR NOT NULL,
+                attack_type attack_enum NOT NULL REFERENCES attack_name(name)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT,
                 target_ip VARCHAR NOT NULL,
                 start_time TIMESTAMP NOT NULL,
-                duration_sec INT NOT NULL,
-                peak_dbs BIGINT NOT NULL,
-                source_count INT NOT NULL
+                duration_sec INT NOT NULL CHECK (duration_sec >= 0),
+                peak_dbs BIGINT NOT NULL CHECK (peak_dbs >= 0),
+                source_count INT NOT NULL CHECK (source_count >= 0),
+                is_detected BOOLEAN NOT NULL DEFAULT TRUE,
+                source_countries TEXT[]
             );
         """)
         conn.commit()
@@ -38,7 +57,7 @@ def open_modal():
     dialog = QtWidgets.QDialog(window)
     dialog.setWindowTitle("Добавить запись")
     dialog.setModal(True)
-    dialog.setFixedSize(400, 400)
+    dialog.setFixedSize(400, 480)
 
     # Поля для ввода
     attack_type_edit = QtWidgets.QLineEdit(dialog)
@@ -65,9 +84,28 @@ def open_modal():
     source_count_edit.setPlaceholderText("Source Count")
     source_count_edit.setGeometry(100, 220, 200, 30)
 
-    # Кнопка вставки
+    source_country_edit = QtWidgets.QLineEdit(dialog)
+    source_country_edit.setPlaceholderText("Source Country")
+    source_country_edit.setGeometry(100, 260, 200, 30)
+
+    is_detected_label = QtWidgets.QLabel(dialog)
+    is_detected_label.setText("Is detected?")
+    is_detected_label.setGeometry(160, 300, 200, 30)
+
+    true_radio = QtWidgets.QRadioButton("Yes", dialog)
+    true_radio.setGeometry(120, 330, 100, 30)
+
+    false_radio = QtWidgets.QRadioButton("No", dialog)
+    false_radio.setGeometry(200, 330, 100, 30)
+
+    radio_group = QtWidgets.QButtonGroup(dialog)
+    radio_group.addButton(true_radio)
+    radio_group.addButton(false_radio)
+
+    true_radio.setChecked(True)
+
     btn_insert = QtWidgets.QPushButton("Внести", dialog)
-    btn_insert.setGeometry(150, 280, 100, 30)
+    btn_insert.setGeometry(150, 380, 100, 30)
 
     def insert_record():
         try:
@@ -77,6 +115,8 @@ def open_modal():
             duration_sec = int(duration_sec_edit.text().strip())
             peak_dbs = int(peak_dbs_edit.text().strip())
             source_count = int(source_count_edit.text().strip())
+            source_country = source_country_edit.text().strip()
+            is_detected = True if true_radio.isChecked() else False
 
             if not (attack_type and target_ip and start_time):
                 QtWidgets.QMessageBox.warning(dialog, "Ошибка", "Заполните все поля!")
@@ -84,10 +124,18 @@ def open_modal():
 
             conn = get_connection()
             cursor = conn.cursor()
+
             cursor.execute("""
-                INSERT INTO users (attack_type, target_ip, start_time, duration_sec, peak_dbs, source_count)
-                VALUES (%s, %s, %s, %s, %s, %s);
-            """, (attack_type, target_ip, start_time, duration_sec, peak_dbs, source_count))
+            INSERT INTO attack_name (name)
+            VALUES (%s)
+            ON CONFLICT (name) DO NOTHING
+            """, (attack_type,))
+            cursor.execute("""
+            INSERT INTO users (attack_type, target_ip, start_time, duration_sec,
+            peak_dbs, source_count, is_detected, source_countries)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            """, (attack_type,target_ip, start_time, duration_sec, peak_dbs,
+                  source_count, is_detected, [source_country] if source_country else None))
             conn.commit()
             cursor.close()
             conn.close()
@@ -99,9 +147,50 @@ def open_modal():
     btn_insert.clicked.connect(insert_record)
     dialog.exec_()
 
-# ========================
-# Создаём приложение и главное окно
-# ========================
+def show_table_window():
+    # Создаём немодальное окно прямо в коде
+    table_dialog = QtWidgets.QWidget()
+    table_dialog.setWindowTitle("Таблица пользователей")
+    table_dialog.setGeometry(100, 100, 800, 450)
+
+    # Поле фильтра
+    filter_edit = QtWidgets.QLineEdit(table_dialog)
+    filter_edit.setPlaceholderText("Введите текст для фильтрации...")
+    filter_edit.setGeometry(10, 10, 780, 30)
+
+    # Таблица
+    table_view = QtWidgets.QTableView(table_dialog)
+    table_view.setGeometry(10, 50, 780, 390)
+
+    # Функция для загрузки данных из psycopg2
+    def load_data():
+        filter_text = filter_edit.text().strip()
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        if filter_text:
+            cursor.execute("SELECT * FROM users WHERE attack_type::text ILIKE %s", (f"%{filter_text}%",))
+        else:
+            cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        model = QtGui.QStandardItemModel()
+        model.setHorizontalHeaderLabels(columns)
+
+
+        for row in rows:
+            items = [QtGui.QStandardItem(str(field)) for field in row]
+            model.appendRow(items)
+
+        table_view.setModel(model)
+        cursor.close()
+        conn.close()
+    load_data()
+    filter_edit.textChanged.connect(lambda text: load_data())
+    table_dialog.show()
+    window.table_dialog_window = table_dialog
+
+
 app = QtWidgets.QApplication(sys.argv)
 window = uic.loadUi("ui/main.ui")  # загружаем UI
 window.setWindowTitle("PostgreSQL + Qt5")
@@ -111,6 +200,8 @@ window.setWindowTitle("PostgreSQL + Qt5")
 # ========================
 window.mainButton_createTable.clicked.connect(create_schema_and_tables)
 window.mainButton_insertData.clicked.connect(open_modal)
+window.mainButton_showData.clicked.connect(show_table_window)
+
 
 # ========================
 # Показываем главное окно
